bab29b2ddfc5dae91466eac91a720614262d51ec
jillbowen <jillian.b.short@gmail.com>
2021-04-24 16:00:27 -0700

    began building Days Controller

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::DaysController < ApplicationController
+
+    def index
+        @days = Day.all 
+        render json: @days
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+    
+    def day_params
+        params.require(:day).permit(:date)
+    end
 end
