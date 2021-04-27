af4de6017c2bf4af5cf383f5c55ba02eb93c8fad
jillbowen <jillian.b.short@gmail.com>
2021-04-26 18:45:40 -0700

    fixed seed data for new date formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-demo_day = Day.create(date: Date.today)
+demo_day = Day.create(date: '4/26/21')
 
 demo_mood_1 = Mood.create(happy_rating: 8, relaxed_rating: 7, calm_rating: 4, confidence_rating: 9, awake_rating: 6, journal_entry: 'Demo journal entry for an average day.', day_id: 1)
