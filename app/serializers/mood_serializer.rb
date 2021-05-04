87a8b6c90669fd0701b52c29cf6128f860dbd475
jillbowen <jillian.b.short@gmail.com>
2021-05-03 18:27:43 -0700

    added created at attribute

--- app/serializers/mood_serializer.rb
+++ app/serializers/mood_serializer.rb
@@ -1,3 +1,7 @@
 class MoodSerializer < ActiveModel::Serializer
-  attributes :id, :happy_rating, :relaxed_rating, :calm_rating, :confidence_rating, :awake_rating, :journal_entry, :day_id
+  attributes :id, :created_at, :happy_rating, :relaxed_rating, :calm_rating, :confidence_rating, :awake_rating, :journal_entry, :day_id
+
+  def created_at
+    self.object.created_at.strftime("%I:%M%p")
+  end
 end
